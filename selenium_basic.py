2e610c35e7bad7490c1064cf10e9fb95d8a4d649
Alloucha <pomelochka23@gmail.com>
2020-07-25 14:41:46 +0300

    Selenium 1

--- selenium_basic.py
+++ selenium_basic.py
@@ -1,9 +1,22 @@
 # Import WD lib
 
+
 # Create WD Chrome Object
+
 
 # Open "https://www.seleniumeasy.com/test/bootstrap-alert-messages-demo.html"
 
 # Click on [Normal success message button]
 
 # assert appeared green success message
+
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.seleniumeasy.com/test/bootstrap-alert-messages-demo.html")
+
+driver.find_element_by_id('normal-btn-success').click()
+
+assert "I'm a normal success message." in driver.page_source
+driver.quit()
